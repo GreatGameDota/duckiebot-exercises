bb627adbfd4dea0e4d72f66b0cf2c9275c8ac4b8
GreatGameDota <17858650+GreatGameDota@users.noreply.github.com>
2022-08-03 13:41:40 -0500

    Add solution to Braitenberg agent

--- braitenberg/agent/solution/main.py
+++ braitenberg/agent/solution/main.py
@@ -25,6 +25,23 @@ class Agent:
 		context.info("init()")
 		self.rgb = None
 		self.start_time = None
+		
+		# Compute gradient #
+		h = 480
+		w = 640
+		Y, X = np.ogrid[:h, :w]
+		# Experiment with this!
+		center_x = w // 2
+		center_y = h // 2
+		gradient = np.sqrt((X - center_x)**2 + (Y - center_y)**2) + 2*abs(X - center_x) + 3*abs(Y - h)
+
+		# Scale gradient to be between 0-1
+		maxNum = np.max(gradient)
+		minNum = np.min(gradient)
+		for i in range(gradient.shape[0]):
+			for j in range(gradient.shape[1]):
+				gradient[i][j] = self.rescale(gradient[i][j], minNum, maxNum, 1, 0)
+		self.gradient = gradient
 
 	def on_received_seed(self, data: int):
 		np.random.seed(data)
@@ -44,9 +61,40 @@ class Agent:
 		if self.rgb is None:
 			return 0.0, 0.0
 			
-		pwm_left = 0.15
-		pwm_right = 0.15
+		obs = self.rgb
 		
+		# Convert to hsv for color masking
+		hsv = cv2.cvtColor(obs, cv2.COLOR_RGB2HSV)
+
+		# CHANGE ME
+		lower_hsv = np.array([0, 100, 0])
+		upper_hsv = np.array([40, 255, 255])
+
+		color_mask = cv2.inRange(hsv, lower_hsv, upper_hsv)
+		color_mask[:color_mask.shape[0]//3,:] = 0
+
+		# Experiment with this! (Use the second colab notebook)
+		left_mask = color_mask[:, :color_mask.shape[1] // 2]
+		right_mask = color_mask[:, color_mask.shape[1] // 2:]
+		
+		l_gradient = self.gradient[:, :color_mask.shape[1] // 2]
+		r_gradient = self.gradient[:, color_mask.shape[1] // 2:]
+
+		left_tot = np.sum(left_mask * l_gradient)
+		right_tot = np.sum(right_mask * r_gradient)
+		
+		maxNum = left_mask.shape[0] * left_mask.shape[1] * 7
+		left_tot = self.rescale(left_tot, 0, maxNum, 0, 1) + 0.01
+		right_tot = self.rescale(right_tot, 0, maxNum, 0, 1)
+		
+		pwm_left = 0.05
+		pwm_right = 0.05
+		
+		tot = left_tot - right_tot
+		if tot > 0:
+			pwm_left += self.trim(tot, 0, 0.85)
+		else:
+			pwm_right += self.trim(-tot, 0, 0.85)
 		return pwm_left, pwm_right
 
 	def on_received_get_commands(self, context: Context, data: GetCommands):
@@ -72,6 +120,27 @@ class Agent:
 		
 	def finish(self, context: Context):
 		context.info("finish()")
+		
+	def rescale(self, n, lower1, upper1, lower2, upper2):
+		"""
+		Rescale a number from its bounds to a different bound
+		"""
+		return (n - lower1) / (upper1 - lower1) * (upper2 - lower2) + lower2
+		
+	def trim(self, value, low, high):
+		"""
+		Trims a value to be between some bounds.
+
+		Args:
+			value: the value to be trimmed
+			low: the minimum bound
+			high: the maximum bound
+
+		Returns:
+			the trimmed value
+		"""
+		return max(min(value, high), low)
+
 
 def main():
 	node = Agent()
@@ -80,4 +149,3 @@ def main():
 
 if __name__ == "__main__":
 	main()
-
